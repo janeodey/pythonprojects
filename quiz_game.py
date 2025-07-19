242a2474adb89020671e8948f69619f5841da524
Jane Odey <janeodeys98@gmail.com>
2025-07-19 21:48:34 +0100

    modified the quiz game

--- quiz_game.py
+++ quiz_game.py
@@ -1,48 +1,66 @@
 # python quiz
 
-questions = ("How many elements are in the periodic table?: ", "Which animal lays the largest egg?: ",
-             "What is the most abundant gas in Earth's atmosphere?: ", "How many bones are in the human body?: ", "Which planet in the solar system is the hottest?: ")
+def quiz_game():
+    questions = ("How many elements are in the periodic table?: ", "Which animal lays the largest egg?: ",
+                 "What is the most abundant gas in Earth's atmosphere?: ", "How many bones are in the human body?: ", "Which planet in the solar system is the hottest?: ")
 
-options = (("A. 116", "B. 117", "C. 118", "D. 119"),
-           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
-           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
-           ("A. 206", "B. 207", "C. 208", "D. 209"),
-           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+    options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+               ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+               ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+               ("A. 206", "B. 207", "C. 208", "D. 209"),
+               ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
-answers = ("C", "D", "A", "A", "B")
-guesses = []
-score = 0
-question_num = 0
+    answers = ("C", "D", "A", "A", "B")
+    guesses = []
+    score = 0
+    question_num = 0
 
-for question in questions:
-    print("-----------------------------")
-    print(question)
-    for option in options[question_num]:
-        print(option)
-    guess = input("Enter (A, B, C or D): ").upper()
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        score += 1
-        print("Correct! ")
-    else:
-        print("INCORRECT!")
-        print(f"The correct answer is {answers[question_num]}")
+    while True:
+        for question in questions:
+            print("-----------------------------")
+            print(f"Question {question_num + 1}: {question}")
+            for option in options[question_num]:
+                print(option)
+            guess = input("Enter (A, B, C or D): ").upper()
+            guesses.append(guess)
 
-    question_num = question_num + 1
+            while guess not in ("A", "B", "C", "D"):
+                print("❌invalid value!!!")
+                guess = input("Please enter (A, B, C, D): ").upper()
+                print(guess)
 
-print("-------------------------------")
-print("     RESULT      ")
-print("-------------------------------")
+            if guess == answers[question_num]:
+                score += 1
+                print("🥳Correct! ")
+            else:
+                print("INCORRECT!")
+                print(f"The correct answer is {answers[question_num]}")
 
-print("answer: ", end="")
-for answer in answers:
-    print(answer, end=" ")
-print()
+            question_num = question_num + 1
 
-print("guesses: ", end="")
-for guess in guesses:
-    print(guess, end=" ")
-print()
+        print("-------------------------------")
+        print("     RESULT      ")
+        print("-------------------------------")
 
-score = int(score / len(questions) * 100)
-print(f"Your score is: {score}%")
+        print("answer: ", end="")
+        for answer in answers:
+            print(answer, end=" ")
+        print()
+
+        print("guesses: ", end="")
+        for guess in guesses:
+            print(guess, end=" ")
+        print()
+
+        score = int(score / len(questions) * 100)
+        print(f"Your score is: {score}%")
+
+        try_again = input("Do you want to try again? (yes/no): ")
+        if try_again not in ("yes", "y"):
+            print("👋Goodbye")
+            break
+        question_num = 0
+        score = 0
+
+
+quiz_game()
