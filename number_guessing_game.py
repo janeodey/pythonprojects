591f6a68c1bcb11004e6440f092f35c021867829
Jane Odey <janeodeys98@gmail.com>
2025-07-22 21:37:27 +0100

    added a max_guessing logic

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -9,29 +9,42 @@ highest_num = 100
 answer = random.randint(lowest_num, highest_num)
 guesses = 0
 is_running = True
+max_guessing = 7
 
-while True:
+while is_running:
     guess = input(
         f"Please select a number between {lowest_num} and {highest_num} (please x to cancel): ")
+
     if guess.lower() == "x":
         print("Thank you for playing with us!")
         break
 
     if guess.isdigit():
         guess = int(guess)
-        guesses += 1
 
         if guess < lowest_num or guess > highest_num:
             print("❌ That number is out of range...")
             print(
                 f"Please select a valid number between {lowest_num} and {highest_num}")
+            continue
+
+        guesses += 1
+
+        if guess == answer:
+            print(f"✅ Correct guess! Well done 🎉 Your answer was {answer}")
+            print(f"📊 Number of guesses: {guesses}")
+            # break or
+            is_running = False
+
         elif guess < answer:
-            print("Too low! Try again!")
+            print("⬇️Too low! Try again!")
         elif guess > answer:
-            print("Too high! Try again!")
-        else:
-            print(f"correct guess✨. The answer was {answer}")
-            print(f"number of guesses {guesses}")
+            print("⬆️Too high! Try again!")
+
+        if guesses == max_guessing:
+            print("❌ You've exceeded the number of free tries!")
+            print(f"🎯 The correct answer was {answer}")
+            # break or
             is_running = False
     else:
         print("Invalid")
